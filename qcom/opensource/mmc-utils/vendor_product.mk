b48ba8da3b1374b49c4ba826f1152ae0b59c69ca
Bruno Martins <bgcngm@gmail.com>
2023-10-26 16:56:09 +0100

    qcom: dataipa: Disable ipanat module and kernel-tests
    Change-Id: Ie65e1ae7d32a7dfda6e777afed9ec605ce9fcc94

--- qcom/opensource/mmc-utils/vendor_product.mk
+++
@@ -1,8 +0,0 @@
-# UFS init scripts
-PRODUCT_PACKAGES += init.qti.ufs.rc
-
-# additional debugging on userdebug/eng builds
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-  PRODUCT_PACKAGES += init.qti.ufs.debug.sh
-endif
-
