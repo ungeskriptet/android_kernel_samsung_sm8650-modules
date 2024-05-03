caf91f5f5982c0be1ee9f9e6cd44f6be347d2494
David Wronek <david@mainlining.org>
2024-05-03 19:12:49 +0200

    Do i need this? idk

--- qcom/opensource/commonsys/packages/apps/Bluetooth/tests/Android.mk
+++
@@ -1,7 +0,0 @@
-ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-# Include all makefiles in subdirectories
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
